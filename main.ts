5b488070e033b4ea4b51fff2fe70aa84cf898578
Damilola91 <damilola.wiwoloku@gmail.com>
2025-11-12 09:51:00 +0100

    add: tsconfig

--- main.ts
+++ main.ts
@@ -1,2 +1,14 @@
 import init from "./db"
 import cors from "cors"
+import express from "express"
+
+const server = express()
+server.use(express.json())
+server.use(cors())
+
+const PORT = process.env.PORT || 4154
+
+
+init()
+
+server.listen(PORT, () => console.log(`Server is runnin' on PORT ${PORT}`))
